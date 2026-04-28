eaf49f85571b7589ebfdbec93fd534c36b4513a8
MEHMET TATAR <mehmettatar5821@gmail.com>
2026-04-28 10:41:34 +0300

    Create trackbar_in_openc.py

---
+++ trackbar_in_openc.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+
+def nothing(x):
+    pass
+
+img = np.zeros((512,512,3),np.uint8)
+
+
+cv2.namedWindow("resim")
+
+cv2.createTrackbar("R","resim",0,255,nothing)
+cv2.createTrackbar("G","resim",0,255,nothing)
+cv2.createTrackbar("B","resim",0,255,nothing)
+
+while(1):
+    cv2.imshow("resim",img)
+
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+    r = cv2.getTrackbarPos("R","resim")
+    g = cv2.getTrackbarPos("G","resim")
+    b = cv2.getTrackbarPos("B","resim")
+
+    img[:] = [b,g,r]
+
+
+    cv2.destroyAllWindows
